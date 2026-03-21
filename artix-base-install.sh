152c26de02da48bb1fcfd1e57f3d2dd5392ebcd8
Visdomr <8884541+Visdomr@users.noreply.github.com>
2026-03-20 18:13:22 -0700

    Update artix-base-install.sh

--- artix-base-install.sh
+++ artix-base-install.sh
@@ -117,7 +117,7 @@ cat > /etc/hosts <<EOL
 127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
 EOL
 
-ln -sf /usr/share/zoneinfo/$(tzselect) /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
